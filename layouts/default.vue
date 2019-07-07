4919179f7f8a4a76829f64d982f7160e91269929
Anilson Lopes <nissobmx@gmail.com>
2019-07-07 13:12:21 -0300

    navbar removed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-    <div class="max-w-4xl mx-auto p-4 md:p-10 flex justify-between">
-      <span class="highlight">
-        [quila_brand]
-      </span>
-      <nav class="flex items-center uppercase">
-        <nuxt-link to="/" class="nav__link">
-          Trabalho
-        </nuxt-link>
-        <nuxt-link to="/" class="nav__link ml-5">
-          Sobre
-        </nuxt-link>
-      </nav>
-    </div>
     <div>
       <nuxt />
     </div>
