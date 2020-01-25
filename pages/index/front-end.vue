86b87a4d5a31b284fba155dec4819faf97745612
Anilson Lopes <anilson@doity.com.br>
2020-01-25 13:41:53 -0300

    Update front-end.vue

--- pages/index/front-end.vue
+++ pages/index/front-end.vue
@@ -3,3 +3,37 @@
     <h2>front-end</h2>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Front-end',
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Front-end'
+        },
+        {
+        hid: 'description',
+          name: 'description',
+          content:
+            'Anilson Lopes, front-end, JavaScript, CSS, HTML, VueJS, NuxtJS'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Anilson Lopes, front-end, JavaScript, CSS, HTML, VueJS, NuxtJS'
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://quila.dev/front-end'
+        }
+      ]
+    }
+  }
+}
+</script>
