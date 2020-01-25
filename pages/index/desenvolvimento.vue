af34208f5e9e2cca3781531db0df51d4c436fbd5
Anilson Lopes <anilson@doity.com.br>
2020-01-25 13:41:50 -0300

    Update desenvolvimento.vue

--- pages/index/desenvolvimento.vue
+++ pages/index/desenvolvimento.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="h-screen bg-black text-white flex items-center justify-center">
-    <h2>front-end</h2>
+    <h2>desenvolvimento</h2>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Desenvolvimento',
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'desenvolvimento'
+        },
+        {
+        hid: 'description',
+          name: 'description',
+          content:
+            'Anilson Lopes, desenvolvimento, JavaScript, CSS, HTML, VueJS, NuxtJS'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Anilson Lopes, desenvolvimento, JavaScript, CSS, HTML, VueJS, NuxtJS'
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://quila.dev/desenvolvimento'
+        }
+      ]
+    }
+  },
+  mounted() {
+    // const { top } = this.$el.getBoundingClientRect()
+    // window.scrollTo(0, top)
+  }
+}
+</script>
